026b2fded7774f8bdb92d464b7ed0ab1e95745d4
Yazdan <47779953+yazdanv@users.noreply.github.com>
2024-02-15 11:30:29 +0000

    Updated for 7.1 Api, added pullrequest type

--- step.sh
+++ step.sh
@@ -21,7 +21,7 @@ then
     description="${BITRISE_APP_TITLE} build #${BITRISE_BUILD_NUMBER} ${state}"
 fi
 
-URL="https://${devops_user}:${devops_pat}@dev.azure.com/${devops_organization}/${devops_project}/_apis/git/repositories/${devops_repository_id}/commits/${BITRISE_GIT_COMMIT}/statuses?api-version=5.1"
+URL="https:/${devops_user}:${devops_pat}/@dev.azure.com/${devops_organization}/${devops_project}/_apis/git/repositories/${devops_repository_id}/${devops_object_type}/${devops_repository_id}/statuses?api-version=7.1-preview.1"
 
 HTTP_RESPONSE=$(curl $URL --silent --write-out "HTTPSTATUS:%{http_code}" \
 -H "Content-Type: application/json" \
